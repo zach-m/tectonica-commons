e55ee89968255529d0883577f389a583652d4035
zach-m <zach@tectonica.co.il>
2015-01-15 12:59:17 +0200

    generaslized Hash.java

--- src/main/java/com/tectonica/core/Hash.java
+++ src/main/java/com/tectonica/core/Hash.java
@@ -20,6 +20,7 @@ package com.tectonica.core;
 
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
+import java.util.Arrays;
 
 /**
  * Simple utility class for String-based hashing
@@ -28,25 +29,39 @@ public class Hash
 {
 	public static String md5(String... pieces)
 	{
+		return digested("MD5", pieces);
+	}
+
+	public static String sha256(String... pieces)
+	{
+		return digested("SHA-256", pieces);
+	}
+
+	private static String digested(String algorithm, String... pieces)
+	{
 		try
 		{
+			MessageDigest digest = MessageDigest.getInstance(algorithm);
+			
 			Charset utf = Charset.forName("UTF-8");
-
-			MessageDigest digest = MessageDigest.getInstance("MD5");
 			for (String piece : pieces)
 				digest.update(piece.getBytes(utf));
-
-			byte[] bytes = digest.digest();
-
-			StringBuffer sb = new StringBuffer();
-			for (int i = 0; i < bytes.length; ++i)
-				sb.append(Integer.toHexString((bytes[i] & 0xFF) | 0x100).substring(1, 3));
-
-			return sb.toString();
+			
+			return bytesToHex(digest.digest());
 		}
 		catch (Exception e)
 		{
-			throw new RuntimeException("Unexpected error: " + e.getMessage(), e);
+			throw new RuntimeException("Error digesting: " + Arrays.toString(pieces), e);
 		}
+	}
+
+	public static String bytesToHex(byte[] bytes)
+	{
+		StringBuffer sb = new StringBuffer();
+
+		for (int i = 0; i < bytes.length; ++i)
+			sb.append(Integer.toHexString((bytes[i] & 0xFF) | 0x100).substring(1, 3));
+
+		return sb.toString();
 	}
 }
